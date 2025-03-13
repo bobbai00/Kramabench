69c574762188ef9349511107d596557358a11bfa
vitaglianog <vitaglianog@gmail.com>
2025-03-13 12:18:37 -0400

    Avoid istantiating statically models if not needed

--- fixtures.py
+++ fixtures.py
@@ -3,33 +3,65 @@ from systems.mixture_agent_example import ExamplemixtureAgentSystem
 from systems.reflection_example import ExampleReflectionSystem
 
 
+configs = {
+    "baseline-gpt-4o-mini": {
+        "class": ExampleBaselineSystem,
+        "kwargs": {"model": "gpt-4o-mini"},
+    },
+    "baseline-gpt-4o": {
+        "class": ExampleBaselineSystem,
+        "kwargs": {"model": "gpt-4o"},
+    },
+    "baseline-gemma-2b-it": {
+        "class": ExampleBaselineSystem,
+        "kwargs": {
+            "model": "google/gemma-2b-it",
+        },
+    },
+    "baseline-llama-2-13b-chat-hf": {
+        "class": ExampleBaselineSystem,
+        "kwargs": {
+            "model": "meta-llama/Llama-2-13b-chat-hf",
+        },
+    },
+    "baseline-deepseek-r1-distill-qwen-14b": {
+        "class": ExampleBaselineSystem,
+        "kwargs": {
+            "model": "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B",
+        },
+    },
+    "reflection-gpt-4o-mini": {
+        "class": ExampleReflectionSystem,
+        "kwargs": {
+            "executor": "gpt-4o-mini",
+            "reflector": "gpt-4o-mini",
+        },
+    },
+    "reflection-gpt-4o": {
+        "class": ExampleReflectionSystem,
+        "kwargs": {
+            "executor": "gpt-4o",
+            "reflector": "gpt-4o",
+        },
+    },
+    "mixture-agent-gpt-4o-mini": {
+        "class": ExamplemixtureAgentSystem,
+        "kwargs": {
+            "suggesters": ["gpt-4o-mini", "gpt-4o-mini"],
+            "merger": "gpt-4o-mini",
+        },
+    },
+    "mixture-agent-gpt-4o": {
+        "class": ExamplemixtureAgentSystem,
+        "kwargs": {
+            "suggesters": ["gpt-4o-mini", "gpt-4o-mini"],
+            "merger": "gpt-4o",
+        },
+    },
+}
+
+
 def system_selector(sut: str = None, verbose=False):
     # keep this for system level arguments
-    kwargs = {"verbose": verbose}
-
-    systems = {
-        "baseline-gpt-4o-mini": ExampleBaselineSystem("gpt-4o-mini", **kwargs),
-        "baseline-gpt-4o": ExampleBaselineSystem("gpt-4o", **kwargs),
-        "baseline-gemma-2b-it": ExampleBaselineSystem("google/gemma-2b-it", **kwargs),
-        "baseline-llama-2-13b-chat-hf": ExampleBaselineSystem(
-            "meta-llama/Llama-2-13b-chat-hf", **kwargs
-        ),
-        "baseline-deepseek-r1-distill-qwen-14b": ExampleBaselineSystem(
-            "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B", **kwargs
-        ),
-        "reflection-gpt-4o-mini": ExampleReflectionSystem(
-            {"executor": "gpt-4o-mini", "reflector": "gpt-4o-mini"}, **kwargs
-        ),
-        "reflection-gpt-4o": ExampleReflectionSystem(
-            {"executor": "gpt-4o", "reflector": "gpt-4o"}, **kwargs
-        ),
-        "mixture-agent-gpt-4o-mini": ExamplemixtureAgentSystem(
-            {"suggesters": ["gpt-4o-mini", "gpt-4o-mini"], "merger": "gpt-4o-mini"},
-            **kwargs,
-        ),
-        "mixture-agent-gpt-4o": ExamplemixtureAgentSystem(
-            {"suggesters": ["gpt-4o-mini", "gpt-4o-mini"], "merger": "gpt-4o"}, **kwargs
-        ),
-    }
-
-    return systems[sut]
+    extra_kwargs = {"verbose": verbose}
+    return configs[sut]["class"](**configs[sut]["kwargs"], **extra_kwargs)
