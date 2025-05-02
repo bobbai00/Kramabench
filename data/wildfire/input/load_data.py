5fcb657b57df5b1da3d8c02f78d9daedc16a6752
eugenieshine <yujinglai0707@gmail.com>
2025-05-01 21:31:04 -0400

    add annotation to load data

--- data/wildfire/input/load_data.py
+++ data/wildfire/input/load_data.py
@@ -1,5 +1,8 @@
 import kagglehub
 
+# WeatherEvents_Jan2016-Dec2022.csv needs to be downloaded from Kaggle
+# Due to the size of the dataset, it is not included in the repository.
+
 # Download latest version
 path = kagglehub.dataset_download("sobhanmoosavi/us-weather-events")
 print("Path to us weather events dataset files:", path)
